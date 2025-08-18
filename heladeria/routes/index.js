37cf0ad80d17d85e7d863656f40d668f50c8a1db
Jorge Paz <jorgefpaz011@gmail.com>
2025-08-18 16:54:03 -0600

    created .gitignore

--- heladeria/routes/index.js
+++ heladeria/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HOLA QUE HACE' });
+  res.render('index', { title: 'adios' });
 });
 
 module.exports = router;
