6371b757dae3f56c6f08214414c4045e7b4d0de5
Jorge Paz <jorgefpaz011@gmail.com>
2025-08-19 17:09:30 -0600

    added basic routes at index.js

--- heladeria/routes/index.js
+++ heladeria/routes/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+console
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +8,22 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'adios' });
 });
+
+app.get('/info', (req, res) => {
+  res.send('Esta es una request de GET')
+})
+
+app.post('/', (req, res) => {
+  res.send('Esto es una POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Esto es una PUT request en user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Esto es una DELETE request en user')
+})
+
 
 module.exports = router;
